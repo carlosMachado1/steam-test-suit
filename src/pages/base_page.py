f595bcc5493058df2ab4a8efc05e2d55cdb3c0cc
Carlos Gabriel da Silva Machado <cgdsm.eng18@uea.edu.br>
2023-08-20 23:34:17 -0400

    feat: adicionado tipagens e refactor
    [Base Page]
    	- Feito um refactor, foram criadas duas novas funções
    		- find_elements
    		- find_element
    	- Adicionado a tipagem correta a todos os paramentos das funções
    	- Documentação em inglês das funções desenvolvidas
    [HomePage]
    	- Adicionado uma nova função para recuperar todos os filtros
    	  aplicados à página
    	- Adicionado a tipagem correta à todos os parametros da funções

--- src/pages/base_page.py
+++ src/pages/base_page.py
@@ -1,24 +1,31 @@
-from selenium.webdriver.common.keys import Keys
+from typing import Tuple
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.expected_conditions import AnyDriver
 
 
 class BasePage:
-    def __init__(self, driver) -> None:
+    def __init__(self, driver: AnyDriver) -> None:
         self.driver = driver
 
-    def click(self, by_locator):
+    def click(self, by_locator: Tuple[str, str]):
         """Performs click on web element whose locator is passed to it"""
-        WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(by_locator)
-        ).click()
+        self.find_element(by_locator).click()
 
-    def enter_text(self, by_locator, text):
+    def enter_text(self, by_locator: Tuple[str, str], *text: str):
         """Performs text entry of the passed in text, in a web element whose locator is passed to it"""
-        return (
-            WebDriverWait(self.driver, 10)
-            .until(EC.visibility_of_element_located(by_locator))
-            .send_keys(text)
+        return self.find_element(by_locator).send_keys(*text)
+
+    def find_element(self, by_locator: Tuple[str, str]):
+        """Returns a web element whose locator is passed to it"""
+        return WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(by_locator)
+        )
+
+    def find_elements(self, by_locator: Tuple[str, str]):
+        """Returns all web elements whose locator is passed to it"""
+        return WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_elements_located(by_locator)
         )
 
     def get_title(self) -> str:
--- src/pages/home_page.py
+++ src/pages/home_page.py
@@ -1,14 +1,22 @@
+from typing import List
 from .base_page import BasePage
 from ..locators.search_locator import SearchGameLocator
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.expected_conditions import AnyDriver
 
 
 class HomePage(BasePage):
-    def __init__(self, driver) -> None:
+    def __init__(self, driver: AnyDriver) -> None:
         super().__init__(driver)
         driver.get("https://store.steampowered.com/?")
 
     def fill_search_field(self, game: str) -> None:
+        """Fill the search field with game parameter"""
         self.enter_text(SearchGameLocator.search_field, game)
 
     def perform_search(self) -> None:
+        """Perform a click to start the search"""
         self.click(SearchGameLocator.search_button)
+
+    def get_all_filters(self) -> List[WebElement]:
+        return self.find_elements(SearchGameLocator.search_filters)
