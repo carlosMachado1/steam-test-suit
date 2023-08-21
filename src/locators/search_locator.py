e8b6a71d293c30d8905d1cd71e537ad98e217efe
Carlos Gabriel da Silva Machado <cgdsm.eng18@uea.edu.br>
2023-08-20 23:31:15 -0400

    fix: Agora search_button é um locator que funciona
    Anteriormente o locator search_button não funcionava corretamente.
    Adicionado um novo locator para todos os filtros aplicados à página.

--- src/locators/search_locator.py
+++ src/locators/search_locator.py
@@ -3,5 +3,8 @@ from selenium.webdriver.common.by import By
 
 class SearchGameLocator:
     search_field = (By.ID, "store_nav_search_term")
-    search_button = (By.ID, "store_search_link")
-    # news_search_field = (By., "store_search_link")
+    search_button = (By.XPATH, "//*[@id='store_search_link']")
+    search_filters = (
+        By.ID,
+        "searchtag_tmpl",
+    )
