067a1683afee532a749f93fd85dcf253178c85aa
Carlos Gabriel da Silva Machado <cgdsm.eng18@uea.edu.br>
2023-08-22 12:33:31 -0400

    fix: locator fixado [search_button]
    [fix]
    	O locator não estava encontrando a tag corretamente
    [feat]
    	adicionado um novo locator (search_results) para encontrar a
    	quantidade de resultados encontrados a partir de uma busca

--- src/locators/search_locator.py
+++ src/locators/search_locator.py
@@ -3,8 +3,15 @@ from selenium.webdriver.common.by import By
 
 class SearchGameLocator:
     search_field = (By.ID, "store_nav_search_term")
-    search_button = (By.XPATH, "//*[@id='store_search_link']")
+    search_button = (
+        By.XPATH,
+        "/html/body/div[1]/div[7]/div[6]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div[9]/div[1]/form/div/a/img",
+    )
     search_filters = (
         By.ID,
         "searchtag_tmpl",
+    )
+    search_result = (
+        By.XPATH,
+        "/html/body/div[1]/div[7]/div[6]/form/div[1]/div/div[1]/div[3]/div[1]",
     )
